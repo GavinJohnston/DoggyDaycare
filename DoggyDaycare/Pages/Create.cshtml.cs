ccb02916251affde4ad63e80a82cef0301722315
Gavin J <65034058+GavinJohnston@users.noreply.github.com>
2022-05-03 00:07:33 +0100

    added posting functionality

---
+++ DoggyDaycare/Pages/Create.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DoggyDaycare.Pages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+
+namespace DoggyDaycare.Pages
+{
+	public class CreateModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        private readonly IConfiguration _configuration;
+
+        public CreateModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [BindProperty]
+        public DaycareModel Daycare { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tablecmd = connection.CreateCommand();
+                tablecmd.CommandText =
+                    $"INSERT INTO Daycare(Owner, Name, Date, Duration) VALUES('{Daycare.Owner}','{Daycare.Name}','{Daycare.Date}',{Daycare.Duration})";
+                tablecmd.ExecuteNonQuery();
+                connection.Close();
+            }
+
+            return RedirectToPage("./index");
+        }
+    }
+}
---
+++ DoggyDaycare/Pages/Models/DaycareModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DoggyDaycare.Pages.Models
+{
+	public class DaycareModel
+	{
+		public string Owner { get; set; }
+		public string Name { get; set; }
+		public DateTime Date { get; set; }
+		public int Duration { get; set; }
+	}
+}
+
